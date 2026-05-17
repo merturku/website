a3067dd22567f57d25f858817ccfa32638af2b1e
Mehmet Ertürk Uludaşdemir <merturku@protonmail.com>
2026-05-17 21:18:48 +0300

    auto: memory sync 2026-05-17 21:18

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://monograph.theserverless.dev',
+  site: 'https://merturku.com',
   output: 'static',
   integrations: [sitemap()],
   vite: {
